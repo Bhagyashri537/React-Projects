ed8d3516d41e1eb6b77dba7380478c059a8bf18d
bhagyashri537 <bhagyashrideoghare116@gmail.com>
2023-01-22 09:36:13 +0530

    using js in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,14 +2,17 @@ import './ExpenseItem.css';
 
 
 function ExpenseItem(){
+    const expenseDate = new Date(2023,0,22)
+    const expenseTitle = 'Car Insurance';
     const locationOfExpenditure = 'Nagpur';
+    const expenseAmount = 200
     return (
         <div className='expense-item'>
-            <div>January 21 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
+                <h2>{ expenseTitle}</h2>
                 <h2>{locationOfExpenditure}</h2>
-                <div className='expense-item__price'>$200</div>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     )
