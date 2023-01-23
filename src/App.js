918835711275ef6e2de6a0340a466ea5a4fd68f8
bhagyashri537 <bhagyashrideoghare116@gmail.com>
2023-01-23 11:06:45 +0530

    passing the data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import ExpenseItem from './components/ExpenseItem';
 import './App.css';
 
 function App() {
+
+   const expense = [
+    {
+        id:"e1",    
+        title:"Car Insurance",
+        amount:200,
+        date:new Date(2023,0,11),
+        location :"Nagpur"
+    },
+    {
+        id:"e2",   
+        title:"Television",
+        amount:300,
+        date:new Date(2023,1,12),
+        location :"Sadar,Nagpur"
+    },
+    
+    {
+        id:"e3",
+        title:"Desk",
+        amount:300,
+        date:new Date(2023,1,22),
+        location :"Nagpur Bazar"
+    }
+   ]
+
   return (
     <div>
-      
-      
-      <ExpenseItem></ExpenseItem>
-     
-    </div>
+    {expense.map(item => {
+     return<ExpenseItem 
+       title={item.title} 
+       amount={item.amount} 
+       date={item.date} 
+       location={item.location}>
+       </ExpenseItem>
+    })}
+  </div>
   );
 }
 
