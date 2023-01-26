74efb8506a917e010b7f6ca13c4ad78e55d4d0a8
bhagyashri537 <bhagyashrideoghare116@gmail.com>
2023-01-26 15:07:07 +0530

    useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,27 +1,47 @@
 
-import React from 'react';
-
+import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+//import ExpenseDetail from './ExpenseDetail';
 import './ExpenseItem.css';
 
 
 
+
 function ExpenseItem(props){
+
+   const [title,setTitle]  = useState(props.title)
+   const [amount,setAmount]  = useState(props.amount)
+
    
+
+   const eventHanler = () => {
+    setTitle('updated!')
+    console.log("title!")
+   }
+   const eventHandler2 = () => {
+    setAmount('100')
+
+   }
+
+  
     return (
         <div className='expense-item'>
-          
-            <div>{props.date.toISOString()}</div>
+             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
-                <h2>{props.location}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
+            <h2>{title}</h2>    
+            <div className='expense-item__price'>${amount}</div>
             </div>
+            <button onClick={eventHanler}>Change title</button>
+            <button onClick={eventHandler2}>Change Expense</button>
+            {/* <button onClick={eventHanler2}>Delete Expense</button> */}
         </div>
     )
 }
 
  
- export default ExpenseItem;
+ export default  ExpenseItem
+  
+ 
  
   
 
