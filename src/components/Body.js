6e538d758e152ca5ee757b76ac12e43727ae8aad
bhagyashri <bhagyashrideoghare116@gmail.com>
2023-02-08 08:50:15 +0530

    using useReducer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,30 @@
-import { useState, useEffect} from "react";
+import { useState, useReducer } from "react";
 import Card from "./Card";
+
+const collegeInfoReucer = (state, action) => {
+    if(action.type === 'USER_INPUT'){
+        return {value: action.val, isValid: action.val.includes('college')}
+    }
+    return {value: '' , isValid:true}
+}
 
 const Body = (props) => {
 
     const [enterUser, setEnterUser] = useState('')
     const [enterAge, setEnterAge] = useState('')
-    const [enterCollege, setEnteredCollege] = useState('')
-    const [collegeName, setCollegeName] = useState(false)
+    // const [enterCollege, setEnteredCollege] = useState('')
+    // const [collegeName, setCollegeName] = useState(false)
 
-    useEffect(() => {
-        setCollegeName(
-            enterCollege.includes('college')
-        )
-    },[enterCollege])
+    const [collegeState, dispatchCollegeName] = useReducer(collegeInfoReucer, {
+        value:'',
+        isValid: true
+    })
+
+    // useEffect(() => {
+    //     setCollegeName(
+    //         enterCollege.includes('college')
+    //     )
+    // },[enterCollege])
     
 
     const userNameHandler = (event) => {
@@ -26,25 +38,29 @@ const Body = (props) => {
     }
 
     const collegeHandler = (event) => {
-        setEnteredCollege(event.target.value)
+        //setEnteredCollege(event.target.value)
+        dispatchCollegeName({ type: 'USER_INPUT', val:event.target.value}
+                    
+                 
+            )
        
 
     }
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if(enterUser.trim().length === 0 || enterAge.trim().length === 0 || enterCollege.trim().length ===0){
+        if(enterUser.trim().length === 0 || enterAge.trim().length === 0 || collegeState.value === true){
             return;
         } 
         if(+enterAge <1){
             return;
         }
-        props.onAddUser(enterUser,enterAge,enterCollege)
+        props.onAddUser(enterUser,enterAge,collegeState.value)
         setEnterUser('');
         setEnterAge('');
-        setEnteredCollege('');
-        setCollegeName(true)
-        console.log(collegeName)
+        //setEnteredCollege('');
+        //setCollegeName(true)
+        //console.log(state)
        
     }
     
@@ -57,7 +73,7 @@ const Body = (props) => {
     <label htmlFor="username">Age(Years)</label>
     <input type="number" id="age" value={enterAge} onChange={ageHandler}/>
     <label>College Name</label>
-    <input type="text" id="college" value={enterCollege} onChange={collegeHandler}/>
+    <input type="text" id="college" value={collegeState.value} onChange={collegeHandler}/>
     <button type="submit">Add Users</button>
    </form>
    </Card>
