d9d82a0d6a9feb0e63f9563b4fab46f919c7cd5a
bhagyashri <bhagyashrideoghare116@gmail.com>
2023-02-08 12:38:30 +0530

    context api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import { useState } from "react";
-import Body from "./components/Body";
-import UserList from "./components/Userlist";
+import React, { useState } from 'react';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-function App(){
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
 
-  const [usersList, setUsersList] = useState([])
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
 
-  const userHandler = (userName, userAge, userCollege) =>{
-      setUsersList((preList) => {
-        return [...preList, {name:userName, age:userAge, college:userCollege, id:Math.random().toString()}];
-      })
-  }
   return (
-    <>
-        <Body onAddUser={userHandler}/>
-        <UserList users={usersList}/>
-        </>
+    <React.Fragment>
+      <AuthContext.Provider value={{
+       isLoggedIn : isLoggedIn
+        }}>
+         <MainHeader  onLogout={logoutHandler} />
+       <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
