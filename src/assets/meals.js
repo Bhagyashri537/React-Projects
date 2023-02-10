b02a42b0e99bde438f85c0046f28b1528c2a43d7
bhagyashri <bhagyashrideoghare116@gmail.com>
2023-02-10 20:35:29 +0530

    1-res app  - building header component

---
+++ src/assets/meals.js
@@ -0,0 +1,3 @@
+
+
+export const IMG_URL = "https://cdn.vox-cdn.com/thumbor/Ldodjn19KbRy2Aq9LbIKMRfZ8hE=/0x0:6000x4000/925x925/filters:focal(2520x1520:3480x2480):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/71262429/Le_Fantome.0.jpg"
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import classes from './Header.module.css'
+import { IMG_URL } from '../../assets/meals';
+import { Fragment } from 'react';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+    return (
+        <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+           
+        </header>
+        <div className={classes['main-image']}>
+            <img alt='/' src={IMG_URL}/>
+        </div>
+         
+        </Fragment>
+    )
+}
+
+export default Header;
